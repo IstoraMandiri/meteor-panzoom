3bd7c9df6a145f8df42f358da2980fd27ef798c8
Chris Hitchcott <hitchcott@gmail.com>
2014-09-05 14:23:27 +0800

    use jquery

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "A jQuery plugin for panning and zooming elements using CSS3",
-  version: '1.0.0',
+  version: '1.0.1',
   git: "http://github.com/hitchcott/meteor-panzoom",
   name: 'hitchcott:panzoom'
 });
@@ -9,6 +9,7 @@ Package.on_use(function (api) {
   if(api.versionsFrom) {
     api.versionsFrom('METEOR@0.9.0');
   }
+  api.use('jquery','client');
 
   api.add_files('lib/dist/jquery.panzoom.min.js', ['client']);
 });
