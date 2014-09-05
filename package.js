c61854a3c4954692133ca8b008bc2109752b9fef
Chris Hitchcott <hitchcott@gmail.com>
2014-09-05 14:09:46 +0800

    Packaged for meteor 0.9

--- package.js
+++ package.js
@@ -1,7 +1,14 @@
 Package.describe({
-  summary: "jquery.panzoom"
+  summary: "A jQuery plugin for panning and zooming elements using CSS3",
+  version: '1.0.0',
+  git: "http://github.com/hitchcott/meteor-panzoom",
+  name: 'hitchcott:panzoom'
 });
 
-Package.on_use(function (api, where) {
+Package.on_use(function (api) {
+  if(api.versionsFrom) {
+    api.versionsFrom('METEOR@0.9.0');
+  }
+
   api.add_files('lib/dist/jquery.panzoom.min.js', ['client']);
 });
